4997b01e2c66ccbb072cd77051a904d27fdaa0d1
Jaybird7828 <whitleje@ad.unc.edu>
2025-05-29 23:30:25 -0400

    please work

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/mentalhealth', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/mentalhealth');
 
 module.exports = mongoose.connection;
